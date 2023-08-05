1ba819a6ccb7d6e02c5f91ee77672a34daa0a799
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-05 18:07:23 -0500

    boilerplate app setup

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -68,7 +68,7 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 /**
