c08423871633fd2727ba8de4acc674729b45c1b3
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-06 17:41:46 -0500

    assert that google values will exist

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -44,8 +44,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
           prompt: 'consent',
