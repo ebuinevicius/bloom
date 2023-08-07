b499cd24c69a93efe67c56c32c8b398855f2690f
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-07 11:42:30 -0500

    add redirects to signin button on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import ThemeSelector from './ThemeSelector';
+import { redirect } from 'next/navigation';
+import { useRouter } from 'next/router';
 
 function Navbar() {
   const { data: sessionData } = useSession();
+  const router = useRouter();
 
   return (
     <nav className="bg-gray-100 border-gray-600 border-b-[1px] dark:bg-gray-900">
@@ -13,7 +16,7 @@ function Navbar() {
           <ThemeSelector />
           <button
             className="rounded bg-gray-200 dark:bg-gray-800 px-3 py-1 font-semibold text-black dark:text-white no-underline transition hover:bg-white/20"
-            onClick={sessionData ? () => void signOut() : () => void signIn()}
+            onClick={sessionData ? () => signOut({ callbackUrl: '/' }) : () => router.push('/signin')}
           >
             {sessionData ? 'Sign out' : 'Sign in'}
           </button>
