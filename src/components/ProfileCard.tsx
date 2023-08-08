d3238045e6385cfab0216d9125dbbe5545f97945
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-08 02:51:29 -0500

    Added followers and posts data to user ProfileCard

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ProfileImage from './ProfileImage';
 import { useSession } from 'next-auth/react';
-import { Button } from './Button';
+import { Button } from './buttons/Button';
+import { api } from '~/utils/api';
+import { LoadingSpinner } from './LoadingSpinner';
 
 interface ProfileCardProps {
   onAddNewPost: () => void;
@@ -9,6 +11,12 @@ interface ProfileCardProps {
 
 function ProfileCard({ onAddNewPost }: ProfileCardProps) {
   const { data: session } = useSession();
+
+  if (!session?.user?.id) {
+    return <LoadingSpinner />;
+  }
+
+  const userProfile = api.user.getUserProfile.useQuery(session.user.id);
 
   return (
     <div className="bg-white dark:bg-gray-800 shadow-xl rounded-lg py-3 flex flex-col items-center px-3 justify-center h-full w-full">
@@ -18,16 +26,28 @@ function ProfileCard({ onAddNewPost }: ProfileCardProps) {
         <h2 className="text-center text-lg font-bold text-gray-500 leading-8">{session?.user.email}</h2>
         <div className="flex gap-6 justify-around text-xl">
           <div className="flex flex-col items-center">
-            <p className="text-black dark:text-white">3</p>
-            <p className="text-gray-400">posts</p>
+            {userProfile.isLoading ? (
+              <LoadingSpinner />
+            ) : (
+              <>
+                <p className="text-black dark:text-white">{userProfile.data?._count.posts}</p>
+                <p className="text-gray-500">Posts</p>
+              </>
+            )}
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-black dark:text-white">3</p>
-            <p className="text-gray-400">friends</p>
+            {userProfile.isLoading ? (
+              <LoadingSpinner />
+            ) : (
+              <>
+                <p className="text-black dark:text-white">{userProfile.data?._count.followers}</p>
+                <p className="text-gray-500">Followers</p>
+              </>
+            )}
           </div>
         </div>
         <Button gray={true} small={true} className="w-36 text-xl font-bold">
-          Edit Profile
+          Profile
         </Button>
         <Button onClick={onAddNewPost} small={true} className="w-36 text-xl font-bold">
           New Post
