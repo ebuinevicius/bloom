931aea30a05f66787896ff5339af8449656d90e0
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-07 22:12:22 -0500

    added query infiniteFeed to query posts infinitely as user scrolls

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,4 +7,47 @@ export const postRouter = createTRPCRouter({
     const post = await ctx.prisma.post.create({ data: { content, userId: ctx.session.user.id } });
     return post;
   }),
+
+  infiniteFeed: protectedProcedure
+    .input(z.object({ limit: z.number().optional(), cursor: z.number().optional() }))
+    .query(async ({ ctx, input }) => {
+      const { limit = 10, cursor = 0 } = input;
+      const currentUserId = ctx.session.user.id;
+      const posts = await ctx.prisma.post.findMany({
+        take: limit + 1,
+        skip: cursor,
+        where: {
+          OR: [
+            {
+              user: {
+                follows: {
+                  some: {
+                    id: currentUserId,
+                  },
+                },
+              },
+            },
+            {
+              userId: currentUserId,
+            },
+          ],
+        },
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: 'desc', // Order by the most recent posts
+        },
+      });
+
+      const hasMorePosts = posts.length > limit;
+      if (hasMorePosts) {
+        posts.pop();
+      }
+      return {
+        posts,
+        hasMorePosts,
+        nextCursor: hasMorePosts ? cursor + limit : null,
+      };
+    }),
 });
