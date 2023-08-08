3b4787016745065f6750490acb80b1740b0af8a1
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-07 19:15:00 -0500

    add postRouter to appRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
-import { createTRPCRouter } from "~/server/api/trpc";
+import { createTRPCRouter } from '~/server/api/trpc';
+import { postRouter } from './routers/post';
 
 /**
  * This is the primary router for your server.
@@ -6,6 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  post: postRouter,
 });
 
 // export type definition of API
