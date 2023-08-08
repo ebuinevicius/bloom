47ba6f4187b9fc5f8cbf7800eb8abf9832419599
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-07 19:14:24 -0500

    Homepage now displays the ProfileCard and allows the user to create new posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
 import { getSession, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useState } from 'react';
+import CreatePostModal from '~/components/CreatePostModal';
+import ProfileCard from '~/components/ProfileCard';
 
 export default function Home() {
   // const hello = api.example.hello.useQuery({ text: 'from tRPC' });
   const { data: session, status } = useSession();
-  const router = useRouter();
+  const [isPostModalOpen, setIsPostModalOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsPostModalOpen(false);
+  };
 
   if (status === 'loading') return <p>Loading</p>;
 
-  return <div>{status}</div>;
+  return (
+    <div className="mt-5 grid grid-cols-3 place-items-center">
+      <ProfileCard onAddNewPost={() => setIsPostModalOpen(true)} />
+      {/* Render the modal conditionally */}
+      {isPostModalOpen && <CreatePostModal isOpen={isPostModalOpen} onClose={closeModal} />}
+    </div>
+  );
 }
 
 export async function getServerSideProps(context: any) {
@@ -29,29 +39,3 @@ export async function getServerSideProps(context: any) {
     props: { session },
   };
 }
-/* 
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-  console.log('sessionData:', sessionData);
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? 'Sign out' : 'Sign in'}
-      </button>
-    </div>
-  );
-}
- */
