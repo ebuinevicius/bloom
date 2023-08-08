e9a11d886763b806eb58b6fa0201a1c745bba925
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-07 22:00:19 -0500

    Created LoadingSpinner component

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,15 @@
+import { ArrowPathIcon } from '@heroicons/react/20/solid';
+
+type LoadingSpinnerProps = {
+  big?: boolean;
+};
+
+export function LoadingSpinner({ big = false }: LoadingSpinnerProps) {
+  const sizeClasses = big ? 'w-16 h-16' : 'w-10 h-10';
+
+  return (
+    <div className="flex justify-center p-2">
+      <ArrowPathIcon className={`animate-spin ${sizeClasses}`} />
+    </div>
+  );
+}
