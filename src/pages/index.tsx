3c0fbf40217cb57cc13567b1ac4d2ba2179f9588
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-07 22:11:55 -0500

    Render an infinite list of posts in the middle of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { getSession, useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CreatePostModal from '~/components/CreatePostModal';
+import { PostCard } from '~/components/PostCard';
 import ProfileCard from '~/components/ProfileCard';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { api } from '~/utils/api';
+import { LoadingSpinner } from '~/components/LoadingSpinner';
+import InfinitePostList from '~/components/InfinitePostList';
 
 export default function Home() {
   // const hello = api.example.hello.useQuery({ text: 'from tRPC' });
   const { data: session, status } = useSession();
   const [isPostModalOpen, setIsPostModalOpen] = useState(false);
+  const posts = api.post.infiniteFeed.useInfiniteQuery({}, { getNextPageParam: (lastPage) => lastPage.nextCursor });
 
   const closeModal = () => {
     setIsPostModalOpen(false);
@@ -16,9 +22,19 @@ export default function Home() {
 
   return (
     <div className="mt-5 grid grid-cols-3 place-items-center">
-      <ProfileCard onAddNewPost={() => setIsPostModalOpen(true)} />
-      {/* Render the modal conditionally */}
-      {isPostModalOpen && <CreatePostModal isOpen={isPostModalOpen} onClose={closeModal} />}
+      <div className="self-start">
+        <ProfileCard onAddNewPost={() => setIsPostModalOpen(true)} />
+      </div>
+      <div className="grid-col-start-2 grid-col-end-3 flex gap-2 flex-col w-full px-2">
+        <InfinitePostList
+          posts={posts.data?.pages.flatMap((page) => page.posts)}
+          isError={posts.isError}
+          isLoading={posts.isLoading}
+          hasMore={posts.hasNextPage}
+          fetchNewPosts={posts.fetchNextPage}
+        />
+        {isPostModalOpen && <CreatePostModal isOpen={isPostModalOpen} onClose={closeModal} />}
+      </div>
     </div>
   );
 }
