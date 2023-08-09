56d8423afc8aef2e665526e0f1447219fa94beaf
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-08 21:59:26 -0500

    Now links to user's profile page

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { api } from '~/utils/api';
 import LikeButton from './buttons/LikeButton';
+import Link from 'next/link';
 
 interface PostCardProps {
   post: {
@@ -14,6 +14,7 @@ interface PostCardProps {
     user: {
       name: string | null;
       image: string | null;
+      id: string;
     };
   };
   //onLike: (postId: string) => void;
@@ -25,15 +26,15 @@ export const PostCard: React.FC<PostCardProps> = ({ post }) => {
   const hoursAgo = Math.floor(timeAgo / (1000 * 60 * 60));
 
   return (
-    <div className=" dark:bg-gray-800 p-4 mb-3 w-full rounded shadow-lg border border-gray-200 dark:border-gray-700 space-y-4">
-      <div className="flex items-center space-x-2">
+    <div className=" dark:bg-gray-800 p-4 mb-3 w-full rounded shadow-lg border border-gray-200 dark:border-gray-700 space-y-2">
+      <Link className="flex gap-2 items-center" href={`/profiles/${post.user.id}`}>
         <img src={post.user.image || ''} alt={post.user.name || ''} className="w-10 h-10 rounded-full" />
         <div>
           <h4 className="font-bold">{post.user.name}</h4>
-          <p className="text-gray-600">{hoursAgo}h ago</p>
         </div>
-      </div>
-      <p>{post.content}</p>
+      </Link>
+      <p className="text-gray-600">{hoursAgo}h ago</p>
+      <p className="text-lg font-normal">{post.content}</p>
       <LikeButton post={post} />
     </div>
   );
