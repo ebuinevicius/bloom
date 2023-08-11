41734b45c7ae0bebf60919bbd7d00dc3f8352926
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-11 04:17:38 -0500

    Move InfinitePostList to its own function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
 import { getSession, useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import CreatePostModal from '~/components/CreatePostModal';
-import { PostCard } from '~/components/PostCard';
 import ProfileCard from '~/components/ProfileCard';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { api } from '~/utils/api';
 import { LoadingSpinner } from '~/components/LoadingSpinner';
 import InfinitePostList from '~/components/InfinitePostList';
+import { GetServerSidePropsContext } from 'next';
 
 export default function Home() {
   const { data: session, status } = useSession();
   const [isPostModalOpen, setIsPostModalOpen] = useState(false);
-  const posts = api.post.infiniteFeed.useInfiniteQuery({}, { getNextPageParam: (lastPage) => lastPage.nextCursor });
 
   const closeModal = () => {
     setIsPostModalOpen(false);
@@ -27,20 +25,28 @@ export default function Home() {
         <ProfileCard userId={session?.user.id} onAddNewPost={() => setIsPostModalOpen(true)} />
       </div>
       <div className="grid-col-start-2 grid-col-end-3 flex gap-2 flex-col w-full px-2 self-start">
-        <InfinitePostList
-          posts={posts.data?.pages.flatMap((page) => page.posts)}
-          isError={posts.isError}
-          isLoading={posts.isLoading}
-          hasMore={posts.hasNextPage}
-          fetchNewPosts={posts.fetchNextPage}
-        />
+        <RecentPosts />
         {isPostModalOpen && <CreatePostModal isOpen={isPostModalOpen} onClose={closeModal} />}
       </div>
     </div>
   );
 }
 
-export async function getServerSideProps(context: any) {
+function RecentPosts() {
+  const posts = api.post.infiniteFeed.useInfiniteQuery({}, { getNextPageParam: (lastPage) => lastPage.nextCursor });
+
+  return (
+    <InfinitePostList
+      posts={posts.data?.pages.flatMap((page) => page.posts)}
+      isError={posts.isError}
+      isLoading={posts.isLoading}
+      hasMore={posts.hasNextPage}
+      fetchNewPosts={posts.fetchNextPage}
+    />
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getSession(context);
 
   if (!session) {
