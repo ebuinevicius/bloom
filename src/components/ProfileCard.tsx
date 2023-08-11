ba1b293901994e5dc12532feaa76a516d3460ce8
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-11 04:16:55 -0500

    Move FollowButton into ProfileCard and update follow-related caches pessimistically

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -4,7 +4,6 @@ import { useSession } from 'next-auth/react';
 import { Button } from './buttons/Button';
 import { api } from '~/utils/api';
 import { LoadingSpinner } from './LoadingSpinner';
-import FollowButton from './buttons/FollowButton';
 import Link from 'next/link';
 
 interface ProfileCardProps {
@@ -59,5 +58,68 @@ function ProfileCard({ onAddNewPost, userId }: ProfileCardProps) {
     </div>
   );
 }
+
+interface FollowButtonProps {
+  followerId: string; // Logged-in user's ID
+  followeeId: string; // Profile user's ID
+}
+
+const FollowButton: React.FC<FollowButtonProps> = ({ followerId, followeeId }: FollowButtonProps) => {
+  const { data: isFollowing, isLoading } = api.user.isFollowing.useQuery({ followerId, followeeId });
+  const trpcUtils = api.useContext();
+
+  // Call toggleFollow mutation to update the user's follower count for display on profile
+  const toggleFollow = api.user.toggleFollow.useMutation({
+    onSuccess: async (result) => {
+      // Update profile of user who was followed to display new follower count
+      const updateFollowee: Parameters<typeof trpcUtils.user.getUserProfile.setData>[1] = (oldData) => {
+        if (oldData == null) {
+          return;
+        }
+        let followerCountModifier = oldData.isFollowing;
+        if (oldData.isFollowing) {
+        }
+
+        return {
+          ...oldData,
+          followerCount: oldData.followerCount + 1,
+        };
+      };
+
+      // Update the isFollowing status for the user who followed
+      const updateFollower: Parameters<typeof trpcUtils.user.isFollowing.setData>[1] = (wasFollowing) => {
+        if (wasFollowing == null) {
+          return;
+        }
+
+        return !wasFollowing;
+      };
+
+      trpcUtils.user.getUserProfile.setData({ userId: followerId }, updateFollowee);
+      trpcUtils.user.isFollowing.setData({ followerId: followerId, followeeId: followeeId }, updateFollower);
+    },
+  });
+
+  const handleFollowToggle = async () => {
+    await toggleFollow.mutate({ userId: followeeId });
+  };
+
+  // If initial toggle state is loading return spinner
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <button
+      disabled={toggleFollow.isLoading}
+      className={`rounded py-2 px-3 w-4/6 font-semibold text-xl text-black ${
+        isFollowing ? 'bg-red-500' : 'bg-green-400'
+      }`}
+      onClick={handleFollowToggle}
+    >
+      {isFollowing ? 'Unfollow' : 'Follow'}
+    </button>
+  );
+};
 
 export default ProfileCard;
--- src/components/buttons/FollowButton.tsx
+++ src/components/buttons/FollowButton.tsx
@@ -3,41 +3,66 @@ import React, { useEffect, useState } from 'react';
 import { api } from '~/utils/api';
 import { LoadingSpinner } from '../LoadingSpinner';
 
-interface FollowButtonProps {
-  followerId: string; // Logged-in user's ID
-  followeeId: string; // Profile user's ID
-}
+// interface FollowButtonProps {
+//   followerId: string; // Logged-in user's ID
+//   followeeId: string; // Profile user's ID
+// }
 
-const FollowButton: React.FC<FollowButtonProps> = ({ followerId, followeeId }: FollowButtonProps) => {
-  const { data: isFollowing, isLoading } = api.user.isFollowing.useQuery({ followerId, followeeId });
-  const trpcUtils = api.useContext();
+// const FollowButton: React.FC<FollowButtonProps> = ({ followerId, followeeId }: FollowButtonProps) => {
+//   const { data: isFollowing, isLoading } = api.user.isFollowing.useQuery({ followerId, followeeId });
+//   const trpcUtils = api.useContext();
 
-  const toggleFollow = api.user.toggleFollow.useMutation({
-    onSuccess: async (result) => {
-      console.log(result);
-      await trpcUtils.user.isFollowing.invalidate();
-      await trpcUtils.user.getUserProfile.invalidate();
-    },
-  });
+//   // Call toggleFollow mutation to update the user's follower count for display on profile
+//   const toggleFollow = api.user.toggleFollow.useMutation({
+//     onSuccess: async (result) => {
+//       // Update profile of user who was followed to display new follower count
+//       const updateFollowee: Parameters<typeof trpcUtils.user.getUserProfile.setData>[1] = (oldData) => {
+//         if (oldData == null) {
+//           return;
+//         }
+//         let followerCountModifier = oldData.isFollowing
+//         if (oldData.isFollowing) {
 
-  const handleFollowToggle = async () => {
-    toggleFollow.mutate({ userId: followeeId });
-  };
+//         }
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
+//         return {
+//           ...oldData,
+//           followerCount: oldData.followerCount + 1,
+//         };
+//       };
 
-  return (
-    <button
-      className={`rounded py-2 px-3 w-4/6 font-semibold text-xl text-black ${
-        isFollowing ? 'bg-red-500' : 'bg-green-400'
-      }`}
-      onClick={handleFollowToggle}
-    >
-      {isFollowing ? 'Unfollow' : 'Follow'}
-    </button>
-  );
-};
+//       // Update the isFollowing status for the user who followed
+//       const updateFollower: Parameters<typeof trpcUtils.user.isFollowing.setData>[1] = (wasFollowing) => {
+//         if (wasFollowing == null) {
+//           return;
+//         }
+
+//         return !wasFollowing;
+//       };
+
+//       trpcUtils.user.getUserProfile.setData({ userId: followerId }, updateFollowee);
+//       trpcUtils.user.isFollowing.setData({ followerId: followerId, followeeId: followeeId }, updateFollower);
+//     },
+//   });
+
+//   const handleFollowToggle = async () => {
+//     toggleFollow.mutate({ userId: followeeId });
+//   };
+
+//   if (isLoading) {
+//     return <LoadingSpinner />;
+//   }
+
+//   return (
+//     <button
+//       className={`rounded py-2 px-3 w-4/6 font-semibold text-xl text-black ${
+//         isFollowing ? 'bg-red-500' : 'bg-green-400'
+//       }`}
+//       onClick={handleFollowToggle}
+//     >
+//       {isFollowing ? 'Unfollow' : 'Follow'}
+//     </button>
+//   );
+// };
 
 export default FollowButton;
