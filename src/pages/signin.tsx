253e8c449c1ed620b919979b7d797b714ac12cc8
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-14 17:28:19 -0500

    Added responsive design

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -7,19 +7,19 @@ import { authOptions } from '~/server/auth';
 
 export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div className="grid place-items-center gap-4 p-10">
-      <h1 className="text-4xl font-bold">Sign in to your account.</h1>
-      <div className="flex px-6 py-4 justify-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-3/12">
-        <div className="rounded-lg bg-slate-800 hover:bg-slate-700 dark:bg-white dark:hover:bg-gray-400 w-5/6 flex justify-center">
+    <div className="grid place-items-center gap-4 p-10 text-center">
+      <h1 className="text-2xl lg:text-4xl font-bold">Sign in to your account.</h1>
+      <div className="flex px-6 py-4 justify-center bg-white border border-gray-200 rounded-lg shadow dark:bg-dark-800 dark:border-gray-700 lg:w-4/6 lg:py-10 2xl:w-1/2">
+        <div className="rounded-lg bg-slate-800 hover:bg-slate-700 dark:bg-white dark:hover:bg-gray-400 w-4/6 flex justify-center">
           {Object.values(providers).map((provider) => (
             <div key={provider.name} className="">
               <button
-                className="flex items-center gap-5 text-2xl hover:bg-cool-gray-100 rounded py-2 px-2 text-gray-200 dark:text-gray-600 font-roboto"
+                className="flex items-center gap-5 hover:bg-cool-gray-100 rounded py-2 px-2 text-gray-200 dark:text-gray-600 font-roboto"
                 type="button"
                 onClick={() => signIn(provider.id)}
               >
                 {provider.name == 'Google' ? <GoogleIcon /> : ''}
-                Sign in with {provider.name}
+                <span className="hidden lg:inline-block lg:text-xl">Sign in with {provider.name}</span>
               </button>
             </div>
           ))}
