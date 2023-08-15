8e9211bcf9a54779ada156a7f9773877a28c87b1
ERIK BUINEVICIUS <erikbcode@gmail.com>
2023-08-14 19:30:38 -0500

    Center loading spinner horizontally within parent

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -18,10 +18,14 @@ function ProfileCard({ onAddNewPost, userId }: ProfileCardProps) {
   const { data: userProfile, isLoading } = api.user.getUserProfile.useQuery({ userId });
 
   if (!session?.user?.id || isLoading || !userProfile) {
-    return <LoadingSpinner />;
+    return (
+      <div className="hidden xl:block self-start w-fit h-fit">
+        <LoadingSpinner />
+      </div>
+    );
   }
   return (
-    <div className="bg-white dark:bg-dark-800 shadow-xl rounded-lg py-3 hidden xl:flex xl:flex-col items-center px-3 justify-center h-full w-full">
+    <div className="bg-white dark:bg-dark-800 shadow-xl rounded-lg py-3 hidden xl:flex flex-col items-center px-3 h-fit w-fit self-start">
       <ProfileImage src={userProfile.image} className="h-20 w-20" />
       <div className="p-2 flex items-center flex-col gap-5">
         <h3 className="text-center text-2xl font-bold text-gray-900 dark:text-white leading-8">{userProfile.name}</h3>
